dc7090daed1b260410f1f822e84b6ab2e4006373
John <jhn.hines@gmail.com>
2023-08-07 17:39:01 -0500

    Lowercase Caesar ready - needs upper

--- caesar-cipher.js
+++ caesar-cipher.js
@@ -1,5 +1,48 @@
-function caesarCipher(string, factor) {
+function caesarCipher(word, factor) {
+    let codedWord = "";
+    let letterValue = 0;
+  
+    for (let i = 0; i < word.length; i++) {
+      letterValue = word.charCodeAt(i);
+      console.log(letterValue);
+      let letter = word.charAt(i);
+    
+      if (letterValue+factor > 122) {
+        let newValue = String.fromCharCode(letterValue + factor - 26);
+        console.log(newValue);
+        codedWord += newValue;
+  
+      } else {
+        codedWord += String.fromCharCode(letterValue + factor);
+      }
+    }
+    return codedWord;
 
+
+
+
+        // This if block is for when it is Z
+    //     if (letter === "z" && factor == 1 || letter ===  "Z" && factor == 1) {
+    //         if (letter === "z"){
+    //             letter = "a";
+    //             codedWord += String.fromCharCode((letter.charCodeAt(0)));
+    //         } else {
+    //             letter = "A";
+    //             codedWord += String.fromCharCode((letter.charCodeAt(0)));
+    //         }
+
+    //     } else if (letter === "z" && factor > 1 || letter ===  "Z" && factor > 1) {
+    //         if (letter === "z"){
+    //             letter = "a";
+    //             codedWord += String.fromCharCode((letter.charCodeAt(0) + factor));
+    //         } else {
+    //             letter = "A";
+    //             codedWord += String.fromCharCode((letter.charCodeAt(0) + factor));
+    //         }
+    //     } else {
+    //         codedWord += String.fromCharCode((letter.charCodeAt(0) + factor));
+    //     }
+    // }
 }
 
 module.exports = caesarCipher;
--- unit-tests.test.js
+++ unit-tests.test.js
@@ -4,32 +4,49 @@ const Calculator = require("./calculator");
 const calc = new Calculator();
 const caesarCipher = require("./caesar-cipher"); 
 
-it ('Capitalization Test', () => {
-    expect(capitalize("test")).toBe("Test");
-    expect(capitalize("Test")).toBe("Test");
-    expect(capitalize("tset")).toBe("Tset");
+// it ('Capitalization Test', () => {
+//     expect(capitalize("test")).toBe("Test");
+//     expect(capitalize("Test")).toBe("Test");
+//     expect(capitalize("tset")).toBe("Tset");
+// })
+
+// it ("Reverse String Test", () => {
+//     expect(reverseString("test")).toBe("tset");
+// })
+
+// it ("Calculator - Add Test", () => {
+//     expect(calc.add(2,2)).toBe(4);
+// })
+
+// it ("Calculator - Subtrack Test", () => {
+//     expect(calc.subtract(4,2)).toBe(2);
+// })
+
+// it ("Calculator - Multiply Test", () => {
+//     expect(calc.multiply(4,2)).toBe(8);
+// })
+
+// it ("Calculator - Divide Test", () => {
+//     expect(calc.divide(4,2)).toBe(2);
+// })
+
+// it ("Caesar Cipher - By 1", () => {
+//     expect(caesarCipher("hello", 1)).toBe("ifmmp");
+// })
+
+it ("Caesar Cipher - By 2", () => {
+    expect(caesarCipher("hello", 2)).toBe("jgnnq");
 })
 
-it ("Reverse String Test", () => {
-    expect(reverseString("test")).toBe("tset");
+it ("Caesar Cipher - Z & 0 Test", () => {
+    expect(caesarCipher("z", 0)).toBe("z");
+    expect(caesarCipher("Z", 0)).toBe("Z");
 })
 
-it ("Calculator - Add Test", () => {
-    expect(calc.add(2,2)).toBe(4);
+it ("Caesar Cipher - y & 3", () => {
+    expect(caesarCipher("y", 3)).toBe("b");
 })
 
-it ("Calculator - Subtrack Test", () => {
-    expect(calc.subtract(4,2)).toBe(2);
-})
-
-it ("Calculator - Multiply Test", () => {
-    expect(calc.multiply(4,2)).toBe(8);
-})
-
-it ("Calculator - Divide Test", () => {
-    expect(calc.divide(4,2)).toBe(2);
-})
-
-it ("Caesar Cipher Test", () => {
-    expect(caesarCipher(test, 1)).toBe("uftu");
+it ("Caesar Cipher - Z & 1", () => {
+    expect(caesarCipher("Z", 1)).toBe("A");
 })
